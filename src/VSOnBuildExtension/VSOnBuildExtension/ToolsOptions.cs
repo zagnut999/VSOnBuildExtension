95ce159a1d950fc7d748070cdb5c6672c9292b4d
Nate <nate.pink@gmail.com>
2014-05-28 13:39:15 -0400

    Added Tools Options and trying to pull back

---
+++ src/VSOnBuildExtension/VSOnBuildExtension/ToolsOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+
+namespace CleverMonkeys.VSOnBuildExtension
+{
+    public class ToolsOptions : DialogPage
+    {
+        public bool ShouldRunIisReset { get; set; }
+        public bool ShouldLogToBuildOutput { get; set; }
+    }
+}
